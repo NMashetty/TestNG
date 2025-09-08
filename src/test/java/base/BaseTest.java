ab04b41edbf3e4a47756e6ed3c160805c1b6b0cf
NMashetty <155301711+NMashetty@users.noreply.github.com>
2025-09-08 23:57:31 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,21 +7,32 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 public class BaseTest {
 //    protected WebDriver delegate;
-    protected SelfHealingDriver driver;
-	protected WebDriver delegate;
+  //  protected SelfHealingDriver driver;
+//	protected WebDriver delegate;
+	protected WebDriver driver;
     @BeforeClass
     public void setup() throws IOException {
     	
-    	WebDriverManager.chromedriver().setup();
-    	delegate = new ChromeDriver();    	
-    	driver = SelfHealingDriver.create(delegate);    
+    	// WebDriverManager.chromedriver().setup();
+    	// delegate = new ChromeDriver();    	
+    	// driver = SelfHealingDriver.create(delegate);    
+		String nodeURL = "http://localhost:8085";
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new RemoteWebDriver(new URL(nodeURL), options);
     }
 
     @AfterClass
